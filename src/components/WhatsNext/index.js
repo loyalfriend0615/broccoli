bb4f156ab9d752c938e5a494d472983b1f58a89e
loyalfriend0615 <loyalfriend0615@gmail.com>
2025-02-19 05:01:53 -0500

    whatsnext page

--- src/components/WhatsNext/index.js
+++ src/components/WhatsNext/index.js
@@ -8,6 +8,18 @@ function WhatsNext() {
             <h1>
                 Whats Next
             </h1>
+            <p>
+                🔥 <b>Strengthen the Pack</b> - The coin is already live, but the community is what makes it unstoppable. <br/>
+                🌎 <b>Grow the Community</b> - Because memes mean more when we meme together. <br/>
+                🎉 <b>Have Fun</b>  - Because that's what crypto is all about.
+            </p>
+            <p>
+                This isn't about <b>false promises, fake hype, or overnight riches. <br/>
+                THE Broccoli Coin is here to build something real, something fun, and something legendary. <br/></b>
+                So, are you in? <br/>
+                <b>Buy. Hold. Meme Hard. Stay Loyal. Be THE Broccoli. <br/>
+                THE Broccoli to the moon!</b> 🌕
+            </p>
         </div>
     );
 }
