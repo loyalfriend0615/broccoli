6424dd1c0b04fe29d376d364d0edcd3d29b01ff4
loyalfriend0615 <loyalfriend0615@gmail.com>
2025-02-19 06:42:00 -0500

    copy button

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,12 +6,21 @@ import HisGif from '../../assets/img/he-and-dog.gif';
 
 function Home() {
     return (
-        <div id="homepage">
-            <h1>
-                THE LEGEND OF BROCCOLI CZ'S DOG
-            </h1>
-            <div>
-                <img src={HisGif} alt="he-and-dog" width="527px" height="485px"/>
+        <div>
+            <div id="homepage">
+                <h1>
+                    THE LEGEND OF BROCCOLI CZ'S DOG
+                </h1>
+                <div>
+                    <img src={HisGif} alt="he-and-dog" width="527px" height="485px"/>
+                </div>
+            </div>
+            <div className="copy">
+                <h4>
+                    CA: Broccoli memes are intended to serve as an expression of support for and engagement with the ideals and beliefs embodied by the symbol
+                    "$Broccoli" and the associated artwork. They are not intended to constitute an investment opportunity, investment contract, or security of any kind.
+                </h4>
+                <button>COPY</button>
             </div>
         </div>
     );
--- src/components/WhatsNext/index.js
+++ src/components/WhatsNext/index.js
@@ -4,22 +4,31 @@ import './WhatsNext.css';
 
 function WhatsNext() {
     return (
-        <div id="whatsnext">
-            <h2>
-                Whats Next
-            </h2>
-            <p>
-                🔥 <b>Strengthen the Pack</b> - The coin is already live, but the community is what makes it unstoppable. <br/>
-                🌎 <b>Grow the Community</b> - Because memes mean more when we meme together. <br/>
-                🎉 <b>Have Fun</b>  - Because that's what crypto is all about.
-            </p>
-            <p>
-                This isn't about <b>false promises, fake hype, or overnight riches. <br/>
-                THE Broccoli Coin is here to build something real, something fun, and something legendary. <br/></b>
-                So, are you in? <br/>
-                <b>Buy. Hold. Meme Hard. Stay Loyal. Be THE Broccoli. <br/>
-                THE Broccoli to the moon!</b> 🌕
-            </p>
+        <div>
+            <div id="whatsnext">
+                <h2>
+                    Whats Next
+                </h2>
+                <p>
+                    🔥 <b>Strengthen the Pack</b> - The coin is already live, but the community is what makes it unstoppable. <br/>
+                    🌎 <b>Grow the Community</b> - Because memes mean more when we meme together. <br/>
+                    🎉 <b>Have Fun</b>  - Because that's what crypto is all about.
+                </p>
+                <p>
+                    This isn't about <b>false promises, fake hype, or overnight riches. <br/>
+                    THE Broccoli Coin is here to build something real, something fun, and something legendary. <br/></b>
+                    So, are you in? <br/>
+                    <b>Buy. Hold. Meme Hard. Stay Loyal. Be THE Broccoli. <br/>
+                    THE Broccoli to the moon!</b> 🌕
+                </p>
+            </div>
+            <div className="copy">
+                <h4>
+                    CA: Broccoli memes are intended to serve as an expression of support for and engagement with the ideals and beliefs embodied by the symbol
+                    "$Broccoli" and the associated artwork. They are not intended to constitute an investment opportunity, investment contract, or security of any kind.
+                </h4>
+                <button>COPY</button>
+            </div>
         </div>
     );
 }
