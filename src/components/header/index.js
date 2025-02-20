27c5b8f972c6515495e9a7f23b65806cd3198996
loyalfriend0615 <loyalfriend0615@gmail.com>
2025-02-20 06:57:18 -0500

    navbar modify

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -16,65 +16,53 @@ function Header() {
         <div id="header">
             <nav className="navbar">
                 <ul className="nav-list">
-                    <div className="nav-left">
-                        <li className="nav-item">
-                            <a href='/' id="home">HOME</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <button className="dropdown-toggle">
-                                ABOUT BROCCOLI
-                            </button>
-                            <ul className="dropdown-menu">
-                                <li>
-                                    <a href='/#story'>HIS STORY</a>
-                                </li>
-                                <li>
-                                    <a href='/#photos'>HIS PHOTOS</a>
-                                </li>
-                                <li>
-                                    <a href='/#video'>HIS VIDEO</a>
-                                </li>
-                            </ul>
-                        </li>
-                    </div>
-                    <img src={logo} alt="The Legend of CZ's Dog" width="80px" height="80px" />
-                    <div className="nav-right">
-                        <li className="nav-item dropdown">
-                            <button className="dropdown-toggle">
-                                $BROC
-                            </button>
-                            <ul className="dropdown-menu">
-                                <li>
-                                    <a href='/#hiscoin'>HIS COIN $BROC</a>
-                                </li>
-                                <li>
-                                    <a href='/#whybroc'>WHY $BROC</a>
-                                </li>
-                                <li>
-                                    <a href='/#whatsnext'>WHAT'S NEXT</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li className="nav-item dropdown" id="more">
-                            <button className="dropdown-toggle">
-                                MORE
-                            </button>
-                            <ul className="dropdown-menu">
-                                <li>
-                                    <Link to='/terms-conditions'>TERMS & CONDITIONS</Link>
-                                </li>
-                                <li>
-                                    <Link to='/privacy-policy'>PRIVACY POLICY</Link>
-                                </li>
-                                <li>
-                                    <Link to='/disclaimer'>DISCLAIMER</Link>
-                                </li>
-                                <li>
-                                    <Link to='/contact'>CONTACT US</Link>
-                                </li>
-                            </ul>
-                        </li>
-                    </div>
+                    <li className="nav-logo">
+                        <a href="/">
+                            <img src={logo} alt="dog and boy" />
+                            <span>THE Broccoli Meme</span>
+                        </a>
+                    </li>
+                    <li className="nav-item dropdown">
+                        <button className="dropdown-toggle">
+                            MORE
+                        </button>
+                        <ul className="dropdown-menu">
+                            <li>
+                                <a href='/terms_conditions'>TERMS & CONDITIONS</a>
+                            </li>
+                            <li>
+                                <a href='/privacy_policy'>PRIVACY POLICY</a>
+                            </li>
+                            <li>
+                                <a href='/disclaimer'>DISCLAIMER</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li className="nav-item">
+                        <a href="/contact">
+                            <span>CONTACT US</span>
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="/#hiscoin">
+                            <span>$BROC</span>
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="/#photos">
+                            <span>GALLERY</span>
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="/#story">
+                            <span>THE STORY</span>
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="/">
+                            <span>HOME</span>
+                        </a>
+                    </li>
                     <div className="hamburger-menu" onClick={toggleMenu}>
                         <div className="bar"></div>
                         <div className="bar"></div>
