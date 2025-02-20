05a5a5bc93f295dafbddeda95b96a4e0f8893a14
loyalfriend0615 <loyalfriend0615@gmail.com>
2025-02-20 15:04:02 -0500

    d

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 import twitter from '../../assets/img/twitter.png';
 import telegram from '../../assets/img/telegram.png';
-import pancakeswap from '../../assets/img/pancakeswap.png';
-import icon4 from '../../assets/img/icon4.png';
-import icon5 from '../../assets/img/icon5.png';
+import discord from '../../assets/img/discord.png';
+import bird from '../../assets/img/bird.png';
+import dex from '../../assets/img/dex.png';
+import bunny from '../../assets/img/bunny.png';
+import github from '../../assets/img/github.png';
 
 import './home.css';
 import {copyToClipboard1} from '../../function/function.js';
@@ -23,18 +25,25 @@ function Home() {
                     <h2>Join Our Community</h2>
                     <div>
                         <a href="#">
+                        <img src={twitter} alt="twitter" />
                         </a>
                         <a href="#">
                             <img src={telegram} alt="telegram" />
                         </a>
                         <a href="#">
-                            <img src={pancakeswap} alt="pancakeswap" />
+                            <img src={discord} alt="discord" />
                         </a>
                         <a href="#">
-                            <img src={icon4} alt="icon4" />
+                            <img src={bird} alt="DEX Screener" />
                         </a>
                         <a href="#">
-                            <img src={icon5} alt="icon5" />
+                            <img src={dex} alt="DEX Tools" />
+                        </a>
+                        <a href="#">
+                            <img src={bunny} alt="PancakeSwap" />
+                        </a>
+                        <a href="#">
+                        <img src={github} alt="github" />
                         </a>
                     </div>
                 </div>
