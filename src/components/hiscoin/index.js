426fb9cbccd48b2f2cf899a270c11fbc096f573f
loyalfriend0615 <loyalfriend0615@gmail.com>
2025-02-21 14:17:47 -0500

    final update

--- src/components/hiscoin/index.js
+++ src/components/hiscoin/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-// import HiscoinImage from '../../assets/img/hiscoin.png';
+import HiscoinImage from '../../assets/img/hiscoin.png';
 
 import './hiscoin.css';
 
@@ -21,6 +21,9 @@ function Hiscoin() {
                         Broccoli isn't just a name—it's a legend. A Belgian Malinois with a story that no other meme coin can match. He didn't just appear out ofthin air; he arrived with a passport, a destiny, and the energy of a bull market on steroids.
                         This Broccoli didn't just pop up overnight. He showed up at CZ's doorstep—literally.
                     </p>
+                </div>
+                <div className="why">
+                    <img src={HiscoinImage} alt="his coin img"/>
                 </div>
             </div>
             <div className="legend why" >
